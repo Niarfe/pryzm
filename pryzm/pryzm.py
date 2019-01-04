5c74f25364bf66b7dedd0c080decd6bd5cb81f38
Niarfe <efrain.olivares@gmail.com>
2019-01-04 02:30:13 -0800

    bug fix, escapes do not pile up indefinitely

--- pryzm/pryzm.py
+++ pryzm/pryzm.py
@@ -1,19 +1,27 @@
-#!/usr/bin/env python
-import time
+
 import sys
+import time
+
 from subprocess import Popen, PIPE
 from pryzm.codes import cap27, text_attributes
+
 class Pryzm(object):
+    """
+        ASCII FORMAT:
+            ESC[AT;FG;AT;BGm<text>ESC[0m
+    """
     _cap27 = cap27
     _text_attributes = text_attributes
-    def __init__(self, subject=None):
-        self.subject = subject if subject else ''
+    def __init__(self, text=None):
+        self.text = text if text else ''
+        self.subject = text
+        self.at = 1
+        self.fg = 30
+        self.bg = 40
+        self.CLR = u"\u001b[0m"
+
         for key, val in self._text_attributes.items():
             self._add_color(key, val)
-        # for key, val in self._clear.items():
-        #     self._add_clear(key, val)
-        # for key, val in self._cursor.items():
-        #     self._add_cursor(key, val)
 
     def _add_color(self, color, code):
         """Add dynamic function to insert color code.
@@ -22,8 +30,14 @@ class Pryzm(object):
             return: function, adds a function named 'color' wrapping test with ascii code.
         """
         def fn(self, text=None):
-            self.subject = text if text else self.subject
-            self.subject =  u"\u001b[{0}m{1}".format(code, self.subject)
+            if text: self.text = text
+            if code >= 40:
+                self.bg = code
+            elif code >= 30:
+                self.fg = code
+            elif code <= 10:
+                self.at = code
+            print(code)
             return self
 
         setattr(Pryzm, color, fn)
@@ -31,31 +45,14 @@ class Pryzm(object):
         fn.__name__ = color
         fn.__doc__ = "Apply {0} to text".format(color)
 
-    def set_position(self, x, y):
-        sys.stdout.write(u"\u001b[{0};{1}H".format(str(x), str(y)))
-
     def show(self):
-        return self.subject+u"\u001b[0m"
+        return u"\u001b[{};{};1;{}m{}{}".format(self.at, self.fg, self.bg, self.text, self.CLR)
 
     def print(self):
-        print(self.subject+u"\u001b[0m")
+        print(self.show())
 
     def __str__(self):
-        return self.subject
+        return self.show()
 
-    def encode(self, codes, display_text):
-        """Convenience method, format codes into ascii wrapper around text.
-            This does NO checking, it simply formats.
-            codes: list of codes, for example [31, 47] # 31 red text, 47 white bg
-            return: string of formated text, for example u"\u001b[31;47 \u001b[0m"
-        """
-        codec = ";".join([str(idx) for idx in codes])
-        return u"\u001b[{0}m{1}\u001b[0m".format(codec, display_text)
-
-
-
-
-
-
-
-
+    def __repr__(self):
+        return self.show()
